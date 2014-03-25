3a91f294153f9ef3c04c9ffeb9bc2795325780fd
Mike Subelsky <github@mikeshop.net>
2014-03-24 20:50:50 -0400

    add autoscaler to save costs

--- Gemfile
+++ Gemfile
@@ -224,3 +224,5 @@ group :profile do
   # if you need to profile, uncomment out this line
   # gem 'rack-perftools_profiler', require: 'rack/perftools_profiler', platform: :mri_19
 end
+
+gem 'autoscaler', require: false
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,31 @@
 sidekiq_redis = { url: $redis.url, namespace: 'sidekiq' }
 
-Sidekiq.configure_server do |config|
-  config.redis = sidekiq_redis
-end
-
 Sidetiq.configure do |config|
   # we only check for new jobs once every 5 seconds
   # to cut down on cpu cost
   config.resolution = 5
 end
 
-Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
-Sidekiq.logger.level = Logger::WARN
+Sidekiq.logger.level = Logger::INFO
+
+if Rails.env.production?
+  require 'autoscaler/sidekiq'
+  require 'autoscaler/heroku_scaler'
+
+  Sidekiq.configure_server do |config|
+    config.redis = sidekiq_redis
+    config.server_middleware do |chain|
+      chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new('sidekiq'), 60)
+    end
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = sidekiq_redis
+    config.client_middleware do |chain|
+      chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new('sidekiq')
+    end
+  end
+else
+  Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
+  Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
+end
